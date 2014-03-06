04f342c8dc0f7529615e4f3b8822be0d67541168
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-03-06 15:10:58 -0500

    Add highlight recolor when renaming annotated file

--- mv-annotated-to-main.py
+++ mv-annotated-to-main.py
@@ -5,6 +5,7 @@ import os
 import os.path
 import re
 
+import recolor
 
 REGEX = re.compile(r'^(.*)_\d{4}-\d{1,2}-\d{1,2}_\d{1,2}-\d{1,2}-\d{1,2}.pdf$')
 
@@ -23,6 +24,12 @@ if __name__ == '__main__':
     PARSER.add_argument(
         '-r', help='Descend recursively into directories.',
         action='store_true', default=False)
+    PARSER.add_argument(
+        '-c', '--highlight-color',
+        help='Change the color of highlight annotations to the given color. '
+        'Specify `nochange` to preserve color. The color string is NOT '
+        'validated and has to be a valid PDF color.',
+        nargs=1, default=['1. 1. .5'])
     PARSER.add_argument('filenames', nargs='*', help='Files to process.')
     ARGS = PARSER.parse_args()
 
@@ -32,4 +39,6 @@ if __name__ == '__main__':
                 for sub_filename in sub_filenames:
                     process(os.path.join(dirpath, sub_filenames))
         else:
+            if ARGS.highlight_color[0] != 'nochange':
+                recolor.process_file(name, ARGS.highlight_color[0])
             process(name)
